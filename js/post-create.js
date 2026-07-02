fddfb8d9a1faa63e324a34866330825e4b3bf0de
Soyeong Park <mb535622@gmail.com>
2026-07-02 14:18:21 +0900

    feat: 게시글 추가 API 연동 및 버튼 활성화 조건 처리 로직 변경

--- js/post-create.js
+++ js/post-create.js
@@ -3,29 +3,61 @@ const postContentInput = document.querySelector('#content');
 const fileButton = document.querySelector('.file-button');
 const postSubmitButton = document.querySelector('.post-submit-button');
 
-const helperGroup = document.querySelector('.post-form-group');
+const helperGroup = postContentInput.closest('.post-form-group');
 const postCreateHelperText = helperGroup.querySelector('.helper-text');
 
-if (postTitleInput.value.trim() !== '' && postContentInput.value.trim() !== '') {
-    postSubmitButton.disabled = false;
-} else {
-    postSubmitButton.disabled = true;
+const validationState = {
+    postTitle: false,
+    postContent: false,
+};
+
+function updatePostSubmitButtonState() {
+    postSubmitButton.disabled = !(
+        validationState.postTitle &&
+        validationState.postContent
+    );
 }
+
+postTitleInput.addEventListener('blur', () => {
+    validationState.postTitle = postTitleInput.value.trim() !== '';
+    updatePostSubmitButtonState();
+});
+
+postContentInput.addEventListener('blur', () => {
+    validationState.postContent = postContentInput.value.trim() !== '';
+    updatePostSubmitButtonState();
+});
 
 // 파일 선택 버튼 클릭 이벤트 => 컴퓨터에서 이미지 파일 업로드
 
-postSubmitButton.addEventListener('click', () => {
+postSubmitButton.addEventListener('click', async() => {
     const postTitle = postTitleInput.value;
     const postContent = postContentInput.value;
 
     postCreateHelperText.textContent = ''; // 초기화
     helperGroup.classList.remove('is-error'); // 초기화
 
+    // 버튼 클릭시 제목, 내용이 작성되어 있지 않을 경우
     if (postTitle.trim() === '' || postContent.trim() === '') {
         postCreateHelperText.textContent = '* 제목, 내용을 모두 작성해주세요.';
         helperGroup.classList.add('is-error');
         return;
     }
     
-    // 게시물 추가 API 호출
+    try {
+        const response = await fetch('http://localhost:8080/posts', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                title: postTitle,
+                content: postContent,
+            }),
+        });
+
+        window.location.href = './posts.html';
+    } catch (error) {
+        console.error(error);
+    }
 });
