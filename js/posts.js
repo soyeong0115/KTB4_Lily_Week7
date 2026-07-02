3e83842ef39bf7fb45a71f9e2f49a4ccdba7d64e
Soyeong Park <mb535622@gmail.com>
2026-07-02 15:33:58 +0900

    feat: 게시글 목록 조회 API 연동

---
+++ js/posts.js
@@ -0,0 +1,62 @@
+const postList = document.querySelector('.post-list');
+
+async function fetchPosts() {
+    const userId = localStorage.getItem('userId');
+
+    try {
+        const response = await fetch('http://localhost:8080/posts', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-USER-ID': userId,
+            },
+        });
+
+        const data = await response.json();
+
+        if (!response.ok) {
+            postList.innerHTML = '<p>게시글을 불러오는데 실패했습니다.</p>';
+            return;
+        }
+
+        renderPosts(data.data);
+        console.log('게시글 목록 응답:', data);
+        console.log('게시글 목록 데이터:', data.data);
+    }
+    catch (error) {
+        console.error(error);
+    }
+}
+
+function renderPosts(posts) {
+    if (posts.length === 0) {
+        postList.innerHTML = '<p>게시글이 없습니다.</p>';
+        return;
+    }
+
+    postList.innerHTML = posts.map((post) => {
+        return `
+            <a class="post-card">
+                <div class="post-card-content">
+                    <h3>${post.title}</h3>
+
+                    <div class="post-info-row">
+                        <p class="post-stats-text">
+                            좋아요 ${post.likeCount}&nbsp;&nbsp;
+                            댓글 ${post.commentCount}&nbsp;&nbsp;
+                            조회수 ${post.viewCount}
+                        </p>
+                        <time>${post.createdAt}</time>
+                    </div>
+                </div>
+
+                <div class="post-author">
+                    <div class="author-image"></div>
+                    <strong>${post.nickname}</strong>
+                </div>
+            </a>
+        `;
+    }).join('');
+}
+
+fetchPosts();
