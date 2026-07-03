4125b4f9247ac67130bd133b6842e5a56930f82f
Soyeong Park <mb535622@gmail.com>
2026-07-03 19:15:52 +0900

    feat: 회원 정보 조회/수정 API 연동 및 유효성 검사 로직 구현

---
+++ js/profile-edit.js
@@ -0,0 +1,104 @@
+const emailText = document.querySelector('#email');
+const nicknameInput = document.querySelector('#nickname');
+
+const helperText = document.querySelector('.profile-helper-text');
+
+const profileSubmitButton = document.querySelector('.profile-submit-button');
+const profileWithdrawButton = document.querySelector('.profile-withdraw-button');
+const profileCompleteToast = document.querySelector('.profile-complete-toast');
+
+const userId = localStorage.getItem('userId');
+
+let originalNickname = ''; // 기존 닉네임 저장하고 시작
+
+async function fetchMyProfile() {
+    if (!userId) {
+        alert('로그인이 필요합니다.');
+        window.location.href = './index.html';
+    } 
+
+    try {
+        const response = await fetch('http://localhost:8080/user/profile', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-USER-ID': userId,
+            },
+        });
+
+        const data = await response.json();
+
+        console.log('프로필 조회 응답:', data);
+
+        if (!response.ok) {
+            alert('회원정보를 불러오지 못했습니다.');
+            return;
+        }
+
+        const profile = data.data;
+
+        emailText.textContent = profile.email;
+        nicknameInput.value = profile.nickname;
+
+        originalNickname = profile.nickname;
+
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+fetchMyProfile();
+
+profileSubmitButton.addEventListener('click', async () => {
+    const nickname = nicknameInput.value.trim();
+
+    // 닉네임 입력하지 않을 시
+    if (nickname == '') {
+        helperText.textContent = '* 닉네임을 입력해주세요.';
+        return;
+    }
+
+    // 닉네임 11자 이상 작성 시
+    if (nickname.length > 10) {
+        helperText.textContent = '* 닉네임은 최대 10자 까지 작성 가능합니다.';
+        return;
+    }
+
+    try {
+        const response = await fetch(`http://localhost:8080/user/profile`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'X-USER-ID': userId,
+            },
+            body: JSON.stringify({
+                nickname: nickname,
+            })
+        });
+
+        const data = await response.json();
+            
+        // 닉네임 중복 시
+        if (!response.ok) {
+            if (data.message === 'nickname_duplicated') {
+                helperText.textContent = '* 중복된 닉네임입니다.';
+                return;
+            }
+        }
+        
+        helperText.textContent = '';
+        originalNickname = nickname;
+
+        console.log('프로필 수정 성공 여부', response.ok);
+
+        // 수정 완료 토스트
+        profileCompleteToast.classList.add('is-show');
+    
+        setTimeout(() => {
+            profileCompleteToast.classList.remove('is-show');
+        }, 1500);
+
+    } catch (error) {
+        console.error(error);
+    }
+});
