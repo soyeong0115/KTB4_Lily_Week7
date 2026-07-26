9c2522915c6c727ef98c4ff48bcc2f4544cc95fc
Soyeong Park <mb535622@gmail.com>
2026-07-27 00:42:45 +0900

    feat: AuthMenu 컴포넌트 구현 (로그인/로그아웃 분기)

--- web/src/components/layout/AuthMenu.jsx
+++ web/src/components/layout/AuthMenu.jsx
@@ -1,4 +1,15 @@
+import { Link } from 'react-router-dom';
+import { useAuth } from '../../hooks/useAuth';
+
 export default function AuthMenu() {
-    // TODO: 로그인 상태에 따라 AuthLinks | ProfileDropdown 분기
-    return null;
+    const { isLoggedIn, logout } = useAuth();
+    // TODO: 로그인 상태에 따라 ProfileDropdown 분기
+    return isLoggedIn ? (
+        <button onClick={logout}>로그아웃</button>
+    ) : (
+        <>
+            <Link to="/login">로그인</Link>
+            <Link to="/signup">회원가입</Link>
+        </>
+    );
 }
