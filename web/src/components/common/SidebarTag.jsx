e1d6a013fbfdd462af3fb7158e4afac63bc07379
Soyeong Park <mb535622@gmail.com>
2026-07-27 00:42:27 +0900

    feat: SidebarTag 컴포넌트 구현

--- web/src/components/common/SidebarTag.jsx
+++ web/src/components/common/SidebarTag.jsx
@@ -1,4 +1,5 @@
-export default function SidebarTag() {
-    // TODO: SidebarTag 구현
-    return null;
+export default function SidebarTag({ color, children }) {
+    return (
+        <span className={`sidebar-tag ${color}`}>{children}</span>
+    );
 }
