835423066fa4c7d6a7ca2790eea7053bc69df4f8
Soyeong Park <mb535622@gmail.com>
2026-07-28 17:25:01 +0900

    feat: PostsPage에서 Header/PostSidebar/PostList 조립 및 Contributors 상태 관리 구현

--- web/src/pages/PostsPage.jsx
+++ web/src/pages/PostsPage.jsx
@@ -1,4 +1,40 @@
+import { useRef, useState } from "react";
+import Header from "../components/layout/Header";
+import PostSidebar from "../components/post/PostSidebar";
+import PostList from "../components/post/PostList";
+
 export default function PostsPage() {
-    // TODO: PostsPage 구현
-    return null;
+    const [ contributors, setContributors ] = useState([]);
+    const seenContributorsRef = useRef(new Set());
+
+    function handlePostsFetched(newPosts) {
+        const newContributors = [];
+
+        newPosts.forEach(post => {
+            const { userId, nickname, profileImage } = post.writer;
+
+            if (!seenContributorsRef.current.has(userId) && seenContributorsRef.current.size + newContributors.length < 8) {
+                seenContributorsRef.current.add(userId);
+                newContributors.push({ userId, nickname, profileImage });
+            }
+        });
+
+        if (newContributors.length > 0) {
+            setContributors((prev) => [...prev, ...newContributors]);
+        }
+    }
+
+    return (
+        <>
+            <Header />
+            <main className="posts-main">
+                <div className="posts-layout">
+                    <PostSidebar contributors={contributors} />
+                    <section className="post-list">
+                        <PostList onPostsFetched={handlePostsFetched} />
+                    </section>
+                </div>
+            </main>
+        </>
+    );
 }
