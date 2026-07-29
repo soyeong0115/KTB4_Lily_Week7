05f94c4e35314d7bce38d9513263935ae68fb5d5
Soyeong Park <mb535622@gmail.com>
2026-07-29 18:47:19 +0900

    chore: 완료된 TODO 주석 제거

--- web/src/components/ProtectedRoute.jsx
+++ web/src/components/ProtectedRoute.jsx
@@ -2,7 +2,6 @@ import { Navigate } from "react-router-dom";
 import { useAuth } from "../hooks/useAuth";
 
 export default function ProtectedRoute({ children }) {
-    // TODO: accessToken 없으면 로그인 페이지로 리다이렉트
     const { isLoggedIn } = useAuth();
     
     return isLoggedIn ? children : <Navigate to="/login" />;
