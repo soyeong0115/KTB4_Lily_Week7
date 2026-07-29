f1c3280259ec14f08001954c8a38a61ce30b5691
Soyeong Park <mb535622@gmail.com>
2026-07-29 09:30:02 +0900

    feat: CommentList 컴포넌트 구현

--- web/src/components/comment/CommentList.jsx
+++ web/src/components/comment/CommentList.jsx
@@ -1,4 +1,26 @@
-export default function CommentList() {
-    // TODO: CommentList 구현
-    return null;
+import CommentItem from './CommentItem.jsx';
+
+export default function CommentList({ comments, postId, onEditRequest, onChanged }) {
+    if (comments.length === 0) {
+        return (
+            <div className="comment-empty">
+                <img className="comment-empty-icon" src="/svg/sad.svg" alt="" />
+                <p>아직 댓글이 없어요.<br />가장 먼저 이야기를 남겨보세요!</p>
+            </div>
+        );
+    }
+
+    return (
+        <section className="comment-list">
+            {comments.map((comment) => (
+                <CommentItem
+                    comment={comment}
+                    postId={postId}
+                    onEditRequest={onEditRequest}
+                    onChanged={onChanged}
+                    key={comment.commentId}
+                />
+            ))}
+        </section>
+    );
 }
