8877778d593003816738b05aee35589e71b40fa0
Soyeong Park <mb535622@gmail.com>
2026-07-29 19:09:10 +0900

    fix: 댓글 없음 상태에도 comment-list 여백 적용되도록 수정

--- web/src/components/comment/CommentList.jsx
+++ web/src/components/comment/CommentList.jsx
@@ -3,10 +3,12 @@ import CommentItem from './CommentItem.jsx';
 export default function CommentList({ comments, postId, onEditRequest, onChanged }) {
     if (comments.length === 0) {
         return (
-            <div className="comment-empty">
-                <img className="comment-empty-icon" src="/svg/sad.svg" alt="" />
-                <p>아직 댓글이 없어요.<br />가장 먼저 이야기를 남겨보세요!</p>
-            </div>
+            <section className="comment-list">
+                <div className="comment-empty">
+                    <img className="comment-empty-icon" src="/svg/sad.svg" alt="" />
+                    <p>아직 댓글이 없어요.<br />가장 먼저 이야기를 남겨보세요!</p>
+                </div>
+            </section>
         );
     }
 
