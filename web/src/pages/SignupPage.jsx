5714bdc10ad2da051dacaa4f44d55c496a15fc51
Soyeong Park <mb535622@gmail.com>
2026-07-29 22:59:22 +0900

    fix: 회원가입 비밀번호 확인 일치해도 헬퍼텍스트가 안 지워지던 버그 수정

--- web/src/pages/SignupPage.jsx
+++ web/src/pages/SignupPage.jsx
@@ -72,6 +72,7 @@ export default function SignupPage() {
             setIsPasswordCheckValid(false);
         } else {
             setPasswordError('');
+            setPasswordCheckError('');
             setIsPasswordCheckValid(true);
         }
     }
