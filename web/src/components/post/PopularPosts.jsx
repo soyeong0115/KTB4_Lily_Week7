b994cae360c25110831d2f80ed466c3871356e0b
Soyeong Park <mb535622@gmail.com>
2026-08-03 16:10:06 +0900

    feat: 인기글 섹션 UI 구현

--- web/src/components/post/PopularPosts.jsx
+++ web/src/components/post/PopularPosts.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
-import { request } from "../../api/client";
-import PostCard from "./PostCard.jsx";
+import { Link } from "react-router-dom";
+import { API_BASE_URL, request } from "../../api/client";
+import { getAvatarColor } from "../../utils/avatarColor.js";
 
 export default function PopularPosts() {
     const [popularPosts, setPopularPosts] = useState([]);
+    const [currentIndex, setCurrentIndex] = useState(0);
 
     useEffect(() => {
         async function fetchPopularPosts() {
@@ -27,20 +29,35 @@ export default function PopularPosts() {
         return null;
     }
 
+    const post = popularPosts[currentIndex];
+    const nickname = post.writer.nickname;
+    const bannerStyle = post.postImage
+        ? { backgroundImage: `url(${API_BASE_URL}${post.postImage})` }
+        : { backgroundColor: getAvatarColor(post.postId) };
+
     return (
         <section className="popular-posts">
-            <h2 className="popular-posts-title">
-                <img className="popular-posts-icon" src="/svg/fire.svg" alt="" />
-                인기글
-            </h2>
+            <Link to={`/posts/${post.postId}`} className="popular-posts-banner" style={bannerStyle}>
+                <div className="popular-posts-overlay" />
 
-            <div className="popular-posts-list">
-                {popularPosts.map((post) => (
-                    <div className="popular-posts-item" key={post.postId}>
-                        <PostCard post={post} />
+                <span className="popular-posts-badge">
+                    <img className="popular-posts-icon" src="/svg/fire.svg" alt="" />
+                    인기글
+                </span>
+
+                <div className="popular-posts-content">
+                    <h2 className="popular-posts-post-title">{post.titlePreview}</h2>
+
+                    <div className="popular-posts-author">
+                        <div className="popular-posts-avatar" style={{ '--avatar-color': getAvatarColor(post.writer.userId) }}>
+                            {post.writer.profileImage
+                                ? <img src={`${API_BASE_URL}${post.writer.profileImage}`} alt="" />
+                                : nickname.charAt(0)}
+                        </div>
+                        <span>{nickname}</span>
                     </div>
-                ))}
-            </div>
+                </div>
+            </Link>
         </section>
     );
 }
