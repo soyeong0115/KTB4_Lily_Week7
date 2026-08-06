0a956e4dd4a84ace332d35bb6e0746c1bb507686
Soyeong Park <mb535622@gmail.com>
2026-08-06 16:30:36 +0900

    feat: 알림 모두읽음/모두삭제 기능 구현

--- web/src/pages/NotificationPage.jsx
+++ web/src/pages/NotificationPage.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { getNotifications } from "../api/notificationApi";
+import { deleteAllNotifications, getNotifications, markAllAsRead } from "../api/notificationApi";
 import Header from "../components/layout/Header";
 import { useModal } from "../hooks/useModal";
 import NotificationList from "../components/notification/NotificationList";
@@ -7,7 +7,7 @@ import NotificationList from "../components/notification/NotificationList";
 export default function NotificationPage() {
     const [notifications, setNotifications] = useState([]);
     const [activeTab, setActiveTab] = useState('all');
-    const { isAuthError, showAlertModal, showLoginRequiredModal } = useModal();
+    const { isAuthError, showAlertModal, showLoginRequiredModal, showConfirmModal } = useModal();
 
     async function fetchNotifications() {
         try {
@@ -20,6 +20,45 @@ export default function NotificationPage() {
             }
 
             await showAlertModal({ message: '알림을 불러오지 못했습니다.' })
+            console.error(error);
+        }
+    }
+
+    async function handleMarkAllAsRead() {
+        try {
+            await markAllAsRead();
+            fetchNotifications();
+        } catch(error) {
+            if (isAuthError(error)) {
+                await showLoginRequiredModal();
+                return;
+            }
+
+            await showAlertModal({ message: '모두 읽음 처리에 실패했습니다.'})
+            console.error(error);
+        }
+    }
+
+    async function handleDeleteAll() {
+        const confirmDelete = await showConfirmModal({
+            title: '모든 알림을 삭제하시겠습니까?',
+            message: '삭제한 알림은 복구할 수 없습니다.'
+        });
+
+        if (!confirmDelete) {
+            return;
+        }
+
+        try {
+            await deleteAllNotifications();
+            fetchNotifications();
+        } catch(error) {
+            if (isAuthError(error)) {
+                await showLoginRequiredModal();
+                return;
+            }
+
+            await showAlertModal({ message: '알림 삭제에 실패했습니다.'})
             console.error(error);
         }
     }
@@ -38,6 +77,9 @@ export default function NotificationPage() {
             <main>
                 <button onClick={() => setActiveTab('all')}>전체</button>
                 <button onClick={() => setActiveTab('unread')}>읽지 않음</button>
+
+                <button onClick={handleMarkAllAsRead}>모두 읽음</button>
+                <button onClick={handleDeleteAll}>모두 삭제</button>
 
                 <NotificationList notifications={visibleNotifications} onChanged={fetchNotifications} />
             </main>
