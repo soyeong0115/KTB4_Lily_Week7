e82ec2ee9485f9ebf4c03fba3dac74ff0ebbecb7
Soyeong Park <mb535622@gmail.com>
2026-08-17 17:48:50 +0900

    chore: react-window용 PostRow 어댑터 제거

--- web/src/components/post/PostRow.jsx
+++
@@ -1,9 +0,0 @@
-import PostCard from "./PostCard.jsx";
-
-export default function PostRow({ index, style, posts }) {
-    return (
-        <div style={style}>
-            <PostCard post={posts[index]} />
-        </div>
-    );
-}
